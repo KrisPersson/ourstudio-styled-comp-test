c306bdabcc76e29a86c79f31a87fc8a60036c7b6
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2024-04-11 16:36:53 +0200

    footer

--- styled-components/components/Footer/index.tsx
+++ styled-components/components/Footer/index.tsx
@@ -1,11 +1,24 @@
-import { Wrapper, Inner } from "./wrapper";
+import { Wrapper, Content, Headline, MailToLink, AddressBox } from "./wrapper";
 import { Container } from "../Container/index";
 
 const Footer = () => {
   return (
     <Wrapper aria-label="Page footer">
       <Container>
-        <Inner></Inner>
+        <Content>
+          <Headline>Vill du hitta på något tillsammans med oss?</Headline>
+          <MailToLink
+            href="mailto:hello@ourstudio.se?subject=Hello Our Studio!"
+            title="Send us an e-mail"
+          >
+            hello@ourstudio.se
+          </MailToLink>
+          <AddressBox>
+            Our Studio <br />
+            Kungsgatan 57 <br />
+            411 15 Göteborg
+          </AddressBox>
+        </Content>
       </Container>
     </Wrapper>
   );
